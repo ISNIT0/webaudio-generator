92da8f5332f54e6c849adbcaaaf296897a4c2699
Joe Reeve <joseph.reeve@googlemail.com>
2018-09-04 22:40:15 +0100

    Added MIDI node code generation

--- dist/app.js
+++ dist/app.js
@@ -377,7 +377,6 @@ module.exports = {
         const synth = new Tone.Synth();
 
         WebMidi.enable(function (err) {
-            // https://github.com/djipco/webmidi
             if (err) {
                 alert("WebMidi could not be enabled.");
             }
@@ -420,6 +419,27 @@ module.exports = {
         return `
 // Requires <script src="https://cdn.jsdelivr.net/npm/webmidi"></script>
 // Requires <script src="https://tonejs.github.io/build/Tone.js"></script>
+
+Tone.setContext(audioCtx);
+const ${nodeName} = new Tone.Synth();
+
+WebMidi.enable(function (err) {
+    if (err) {
+        alert("WebMidi could not be enabled.");
+    }
+
+    output = WebMidi.outputs[0];
+    input = WebMidi.inputs[0];
+
+    input.addListener('noteon', "all", e => {
+        output.playNote(e.note.name + e.note.octave, 'all', { velocity: 1 });
+        ${nodeName}.triggerAttack(e.note.name + e.note.octave);
+    });
+    input.addListener('noteoff', "all", e => {
+        output.playNote(e.note.name + e.note.octave, 'all', { velocity: 0 });
+        ${nodeName}.triggerRelease();
+    });
+});
 `;
     }
 }
